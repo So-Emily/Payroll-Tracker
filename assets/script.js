69111c334387f96fdd557ecfd551709acb43156e
So-Emily <esoriano110@gmail.com>
2024-05-23 14:34:00 -0700

    tables showing up and input fixed to stop when canceled

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,23 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   let employeesArray = [];
-  let employeeCount = parseInt(prompt("How many employees would you like to add?"));
-  for (let i = 0; i < employeeCount; i++) {
+  while (true) {
     let employee = {
-      firstName: prompt("Enter employee's first name:"),
-      lastName: prompt("Enter employee's last name:"),
-      salary: parseFloat(prompt("Enter employee's salary:"))
+      firstName: prompt("Enter employee's first name (or press cancel to stop):"),
+      lastName: prompt("Enter employee's last name (or press cancel to stop):"),
+      salary: parseFloat(prompt("Enter employee's salary (or press cancel to stop):"))
     };
+    if (employee.firstName === null || employee.lastName === null || employee.salary === null) {
+      break;
+    }
     employeesArray.push(employee);
   }
+
+  console.log(employeesArray);
+
   return employeesArray; // return the array
-}
+
+  }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
